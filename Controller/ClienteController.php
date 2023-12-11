bbbd1f68a8dbd34ce21ebde5870c8fd3f27bf0c9
Arturo1502 <149508454+Arturo1502@users.noreply.github.com>
2023-12-11 15:35:23 -0500

    prueba

--- Controller/ClienteController.php
+++ Controller/ClienteController.php
@@ -4,7 +4,9 @@ require_once $_SERVER['DOCUMENT_ROOT']. '/Models/clientes.php';
 class ClientesController{
     function index(){
         $cliente = new PedirClientes;
+        
         $clientes = $cliente->all();
+
 
         print_r($clientes);
 
